2bb688231bc665628bf6ca3c1f342facbc919235
James Fenton <jfenton1997@gmail.com>
2019-11-09 14:07:30 +0000

    V0.0.1
    movement and tilemap

---
+++ Hacksoc/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Attributes")]
+    public float playerSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * playerSpeed * Time.deltaTime);
+        
+    }
+}
