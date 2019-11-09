f3ca4f76862716ce12739b3c6a30aaaacbd3980d
James Fenton <jfenton1997@gmail.com>
2019-11-09 15:45:55 +0000

    V0.0.1

---
+++ Hacksoc/HackSoc3d/Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Params")]
+    public float StartSpeed = 500f;
+
+
+    [HideInInspector]
+    public float speed;
+
+    public Rigidbody rb;
+    Vector3 movement;
+     void Start()
+    {
+        speed = StartSpeed;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKey(KeyCode.A))  // If the player is pressing the "d" key
+        {
+
+            // Add a force to the right
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))  // If the player is pressing the "a" key
+        {
+            // Add a force to the left
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))  // If the player is pressing the "d" key
+        {
+            // Add a force to the right
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))  // If the player is pressing the "a" key
+        {
+            // Add a force to the left
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+    }
+}
