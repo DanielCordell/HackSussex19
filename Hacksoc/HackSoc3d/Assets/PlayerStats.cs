089800b9c92f0f0ced06bca6cd4082236f40286a
James Fenton <jfenton1997@gmail.com>
2019-11-09 20:13:54 +0000

    0.0.5
    health, damage

---
+++ Hacksoc/HackSoc3d/Assets/PlayerStats.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class PlayerStats : MonoBehaviour
+{
+
+    public int startHealth = 3;
+    public float iFrames = 10f;
+    [HideInInspector]
+    public static int health;
+    public static float playerSpeed;
+
+    public GameObject healthBar;
+    // Start is called before the first frame update
+
+    private float timeRemaining;
+
+    private bool beenHit;
+    void Awake ()
+    {
+        health = startHealth;
+        beenHit = false;
+    }
+
+    // Update is called once per frame
+    public void takeDamage(int damage = 1)
+    {
+
+        if (!beenHit)
+        {
+            health -= damage;
+            Debug.Log("take damge");
+            healthBar.GetComponent<HealthDisplay>().UpdateDisplay();
+            if (health <= 0)
+            {
+                Debug.Log("gameOver");
+            }
+            beenHit = true;
+            timeRemaining = iFrames;
+        }
+
+    }
+
+    public void Update()
+    {
+        if (beenHit)
+        {
+            
+            if(timeRemaining <= 0)
+            {
+                beenHit = false;
+            }
+
+            timeRemaining -= Time.deltaTime;
+        }
+    }
+}
