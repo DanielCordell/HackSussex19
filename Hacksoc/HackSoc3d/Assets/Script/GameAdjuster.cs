79995f7db4eb2d0a63691b9be4490fb7b722b90f
Daniel Cordell <danielbc@hotmail.co.uk>
2019-11-10 06:45:36 +0000

    Working on stage adjuster

--- Hacksoc/HackSoc3d/Assets/Script/GameAdjuster.cs
+++ Hacksoc/HackSoc3d/Assets/Script/GameAdjuster.cs
@@ -4,11 +4,51 @@ using UnityEngine;
 
 public class GameAdjuster : MonoBehaviour
 {
-    public List<string> commands;
+    public List<string> commands = new List<string>();
+
+    public Dictionary<string, int> commandCount = new Dictionary<string, int>();
+
+    public List<string> names = new List<string>();
+
+    public List<string> phrases = new List<string>();
+
+    public float timeBetweenUpdates;
+
+    void Start()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        commandCount["slow"] = 0;
+        commandCount["fast"] = 0;
+        commandCount["kill"] = 0;
+        commandCount["enemy"] = 0;
+        commandCount["powerup"] = 0;
+    }
 
     public void AddCommands(List<string> commands)
     {
-        this.commands.AddRange(commands);
+        foreach (string command in commands)
+        {
+            if (command == "slow" || command == "fast" || command == "powerup" || command == "kill" || command == "enemy") 
+                commandCount[command]++;
+            else
+            {
+                string cmdStmt = command.Substring(0, command.IndexOf(" "));
+                string cmdEnd = command.Substring(command.IndexOf(" "));
+                if (cmdStmt == "name")
+                {
+                    this.names.Add(cmdEnd);
+                }
+
+                if (cmdStmt == "phrase")
+                {
+                    this.phrases.Add(cmdEnd);
+                }
+            }
+        }
     }
 
     void FixedUpdate()
